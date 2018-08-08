fb23aa6cfc130d4cc7a5a81baab6794b3a7232f8
Jude Ibe <judei@quinnox.com>
2018-08-08 10:23:43 -0500

    create regular_employee security role

--- src/main/java/com/manageme/app/security/AuthoritiesConstants.java
+++ src/main/java/com/manageme/app/security/AuthoritiesConstants.java
@@ -11,6 +11,8 @@ public final class AuthoritiesConstants {
 
     public static final String ANONYMOUS = "ROLE_ANONYMOUS";
 
+    public static final String REGULAR_EMPLOYEE = "ROLE_REGULAR_EMPLOYEE";
+    
     private AuthoritiesConstants() {
     }
 }
