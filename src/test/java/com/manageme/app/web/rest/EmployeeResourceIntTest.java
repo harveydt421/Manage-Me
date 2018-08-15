e07bdb006839cc317af8327b090ce0cd4e7cf047
Jude Ibe <judei@quinnox.com>
2018-08-15 09:18:32 -0500

    add mock user - fixes #31

--- src/test/java/com/manageme/app/web/rest/EmployeeResourceIntTest.java
+++ src/test/java/com/manageme/app/web/rest/EmployeeResourceIntTest.java
@@ -16,6 +16,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -38,6 +39,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
  */
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = ManageMeApp.class)
+@WithMockUser(username="admin", authorities={"ROLE_ADMIN"}, password = "admin")
 public class EmployeeResourceIntTest {
 
     private static final String DEFAULT_PHONE_NUMBER = "AAAAAAAAAA";
