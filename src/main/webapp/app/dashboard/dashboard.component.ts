2e26e052e6a73c1d2576bd2ff8eb291bd82f32d7
Dustin Harvey <dustinh@quinnox.com>
2018-08-28 07:34:13 -0500

    Getting Employee ID for the Dashboard

--- src/main/webapp/app/dashboard/dashboard.component.ts
+++ src/main/webapp/app/dashboard/dashboard.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Principal } from 'app/core';
+import { Subscription } from 'rxjs';
+import { JhiEventManager, JhiAlertService } from 'ng-jhipster';
+import { IEmployee } from 'app/shared/model/employee.model';
+import { EmployeeService } from 'app/entities/employee/employee.service';
+import { HttpErrorResponse, HttpResponse } from '@angular/common/http';
 
 @Component({
     selector: 'jhi-dashboard',
@@ -7,9 +12,16 @@ import { Principal } from 'app/core';
     styles: []
 })
 export class DashboardComponent implements OnInit, OnDestroy {
+    employees: IEmployee[];
+    employee: IEmployee;
     currentAccount: any;
+    eventSubscriber: Subscription;
 
-    constructor(private principal: Principal) {}
+    constructor(
+        private principal: Principal,
+        private jhiAlertService: JhiAlertService,
+        private employeeService: EmployeeService
+        ) {}
 
     // private getPageTitle(routeSnapshot: ActivatedRouteSnapshot) {
     //     let title: string = routeSnapshot.data && routeSnapshot.data['pageTitle'] ? routeSnapshot.data['pageTitle'] : 'manageMeApp';
@@ -19,9 +31,20 @@ export class DashboardComponent implements OnInit, OnDestroy {
     //     return title;
     // }
 
+    loadUser() {
+        this.employeeService.query().subscribe(
+            (res: HttpResponse<IEmployee[]>) => {
+                this.employees = res.body;
+                this.employee = this.employees.filter(employee => employee.user.id === this.currentAccount.id)[0];
+            },
+            (res: HttpErrorResponse) => this.onError(res.message)
+        );
+        }
+
     ngOnInit() {
         this.principal.identity().then(account => {
             this.currentAccount = account;
+            this.loadUser();
         });
         // this.router.events.subscribe(event => {
         //     if (event instanceof NavigationEnd) {
@@ -31,4 +54,8 @@ export class DashboardComponent implements OnInit, OnDestroy {
     }
 
     ngOnDestroy(): void {}
+
+     private onError(errorMessage: string) {
+        this.jhiAlertService.error(errorMessage, null, null);
+    }
 }
