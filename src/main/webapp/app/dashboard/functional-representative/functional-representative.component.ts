da5ef20ff49da815554e1bb1ef38059cd48fdfcd
Dustin Harvey <dustinh@quinnox.com>
2018-08-29 14:08:36 -0500

    adding fr component ts file

---
+++ src/main/webapp/app/dashboard/functional-representative/functional-representative.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'jhi-functional-representative',
+    templateUrl: './functional-representative.component.html',
+    styles: []
+})
+export class FunctionalRepresentativeComponent implements OnInit {
+    constructor() {}
+
+    ngOnInit() {}
+}
