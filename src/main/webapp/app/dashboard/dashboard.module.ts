4e68fe02aed69487d63f1695bcd63d58093e0bed
Dustin Harvey <dustinh@quinnox.com>
2018-08-29 14:02:54 -0500

    Adding Functional Representative Dashboard Component

--- src/main/webapp/app/dashboard/dashboard.module.ts
+++ src/main/webapp/app/dashboard/dashboard.module.ts
@@ -5,13 +5,14 @@ import { HumanResourceComponent } from 'app/dashboard/human-resource/human-resou
 import { dashboardRoute } from 'app/dashboard/dashboard.route';
 import { DashboardComponent } from 'app/dashboard/dashboard.component';
 import { ManageMeSharedModule } from 'app/shared';
+import { FunctionalRepresentativeComponent } from './functional-representative/functional-representative.component';
 
 const LAYOUT_ROUTES = [humanResourceRoute];
 const ENTITY_STATES = [...dashboardRoute];
 
 @NgModule({
     imports: [ManageMeSharedModule, RouterModule.forChild([...ENTITY_STATES, ...LAYOUT_ROUTES])],
-    declarations: [HumanResourceComponent, DashboardComponent],
+    declarations: [HumanResourceComponent, DashboardComponent, FunctionalRepresentativeComponent],
     exports: [RouterModule]
 })
 export class ManageMeDashboardModule {}
