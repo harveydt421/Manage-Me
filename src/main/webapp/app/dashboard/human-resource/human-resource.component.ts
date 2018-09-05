062be12052a46e223946cf2ca9d0a841e5efede9
Cresencio Quereapa <cresencioq@quinnox.com>
2018-09-05 11:55:12 -0500

    HR view/edit Separation Applications
    I copied most of the code from the Funtional Rep dashboard and pasted it on the HR dashboard. However, I am not able to see pending SA on the HR dashboard.

--- src/main/webapp/app/dashboard/human-resource/human-resource.component.ts
+++ src/main/webapp/app/dashboard/human-resource/human-resource.component.ts
@@ -1,12 +1,60 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { HttpErrorResponse, HttpResponse } from '@angular/common/http';
+import { Subscription } from 'rxjs';
+import { JhiEventManager, JhiAlertService } from 'ng-jhipster';
+import { ISeparationApplication } from 'app/shared/model/separation-application.model';
+import { Principal } from 'app/core';
+import { SeparationApplicationService } from 'app/entities/separation-application';
+import { Moment } from 'moment';
 
 @Component({
     selector: 'jhi-human-resource',
     templateUrl: './human-resource.component.html',
     styles: []
 })
-export class HumanResourceComponent implements OnInit {
-    constructor() {}
 
-    ngOnInit() {}
+export class HumanResourceComponent implements OnInit, OnDestroy {
+    separationApplications: ISeparationApplication[];
+    currentAccount: any;
+    eventSubscriber: Subscription;
+
+    constructor(
+        private separationApplicationService: SeparationApplicationService,
+        private jhiAlertService: JhiAlertService,
+        private eventManager: JhiEventManager,
+        private principal: Principal
+    ) {}
+
+    loadAll() {
+        this.separationApplicationService.query().subscribe(
+            (res: HttpResponse<ISeparationApplication[]>) => {
+                this.separationApplications = res.body;
+            },
+            (res: HttpErrorResponse) => this.onError(res.message)
+        );
+    }
+
+    ngOnInit() {
+        this.loadAll();
+        this.principal.identity().then(account => {
+            this.currentAccount = account;
+        });
+        this.registerChangeInSeparationApplications();
+    }
+
+    ngOnDestroy() {
+        this.eventManager.destroy(this.eventSubscriber);
+    }
+
+    trackId(index: number, item: ISeparationApplication) {
+        return item.id;
+    }
+
+    registerChangeInSeparationApplications() {
+        this.eventSubscriber = this.eventManager.subscribe('separationApplicationListModification', response => this.loadAll());
+    }
+
+    private onError(errorMessage: string) {
+        this.jhiAlertService.error(errorMessage, null, null);
+    }
 }
