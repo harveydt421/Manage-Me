f81e30a6601da5cedd2d5c01cba30d0bba1cf7bd
David Simon <davids@quinnox.com>
2018-09-05 14:48:19 -0500

    Show Line Items on FR Dashboard and SA Detail Page

--- src/main/java/com/manageme/app/domain/LineItem.java
+++ src/main/java/com/manageme/app/domain/LineItem.java
@@ -3,7 +3,7 @@ package com.manageme.app.domain;
 import org.hibernate.annotations.Cache;
 import org.hibernate.annotations.CacheConcurrencyStrategy;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import javax.persistence.*;
 
