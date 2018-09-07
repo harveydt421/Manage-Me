785c22e46b3e38ac0e25cff0c256886b20550c06
Dustin Harvey <dustinh@quinnox.com>
2018-09-07 10:45:10 -0500

    Showing Line Items on sep-app detail and fr dashboard

--- src/main/webapp/app/entities/separation-application/separation-application-detail.component.ts
+++ src/main/webapp/app/entities/separation-application/separation-application-detail.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import { ISeparationApplication } from 'app/shared/model/separation-application.model';
+import { LineItemService } from 'app/entities/line-item/line-item.service';
+import { ILineItem } from 'app/shared/model/line-item.model';
+import { HttpErrorResponse, HttpResponse } from '@angular/common/http';
+import { JhiEventManager, JhiAlertService } from 'ng-jhipster';
 
 @Component({
     selector: 'jhi-separation-application-detail',
@@ -9,13 +13,31 @@ import { ISeparationApplication } from 'app/shared/model/separation-application.
 })
 export class SeparationApplicationDetailComponent implements OnInit {
     separationApplication: ISeparationApplication;
+    lineItems: ILineItem[];
 
-    constructor(private activatedRoute: ActivatedRoute) {}
+    constructor(private activatedRoute: ActivatedRoute,
+                private jhiAlertService: JhiAlertService,
+                private lineItemService: LineItemService) {}
+
+    getLineItem() {
+        this.lineItemService.query().subscribe(
+        (res: HttpResponse<ISeparationApplication[]>) => {
+                this.lineItems = res.body;
+        },
+            (res: HttpErrorResponse) => this.onError(res.message)
+        );
+    }
 
     ngOnInit() {
         this.activatedRoute.data.subscribe(({ separationApplication }) => {
             this.separationApplication = separationApplication;
         });
+
+        this.getLineItem();
+    }
+
+     private onError(errorMessage: string) {
+        this.jhiAlertService.error(errorMessage, null, null);
     }
 
     previousState() {
